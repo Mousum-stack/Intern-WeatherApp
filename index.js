d3f57bb81dd03308343369190a5149b25d071481
Mousum-stack <mousumgogoi392@gmail.com>
2023-08-08 23:54:38 +0530

    Updated the javascript file

---
+++ index.js
@@ -0,0 +1,13 @@
+const tempreature = document.querySelector(".weather1");
+const city = document.querySelector(".weather2 p");
+const date = document.querySelector(".weather2 span");
+const emoji = document.querySelector(".weather3 img");
+const weather = document.querySelector(".weather3 span");
+const search = document.querySelector(".searchField");
+const form = document.querySelector("form");
+
+const fetchData = () => {};
+
+
+
+
