04dabd91e61d1a137ea647a78ab19db11e342ae0
Mousum-stack <mousumgogoi392@gmail.com>
2023-08-11 01:00:22 +0530

    Intregrated the weather API

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ const emoji = document.querySelector(".weather3 img");
 const weather = document.querySelector(".weather3 span");
 const search = document.querySelector(".searchField");
 const form = document.querySelector("form");
+let target = "delhi" ;
 
-const fetchData = () => {};
+const fetchData = async() => {
+
+    const url = `https://api.weatherapi.com/v1/current.json?key=cecad235e25a45c1b04174543230808&q=${target}`;
+    
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data);
+};
+fetchData();
 
 
 
